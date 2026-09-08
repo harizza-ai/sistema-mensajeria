e87e16361057837364ba2f97b30078a923543802
Fabian MUñoz Zuñiga <efmunoz@unimayor.edu.co>
2026-09-07 20:49:54 -0500

    feat: clase estado Lectura

--- EstadoLectura.java
+++ EstadoLectura.java
@@ -1,24 +1,3 @@
 public interface EstadoLectura {
     EstadoLectura siguienteEstado();
 }
-
-public class Enviado implements EstadoLectura {
-    @Override
-    public EstadoLectura siguienteEstado() {
-        return new Recibido();
-    }
-}
-
-public class Recibido implements EstadoLectura {
-    @Override
-    public EstadoLectura siguienteEstado() {
-        return new Leido();
-    }
-}
-
-public class Leido implements EstadoLectura {
-    @Override
-    public EstadoLectura siguienteEstado() {
-        return this; // Estado final
-    }
-}
